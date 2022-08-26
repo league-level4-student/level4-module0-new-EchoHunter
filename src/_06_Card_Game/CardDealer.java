711651466531b6a9a4642de6612f3479b15ec6c1
JamesQian123 <Eddiejhunter1000000@gmail.com>
2022-08-25 18:29:10 -0700

    :b:

--- src/_06_Card_Game/CardDealer.java
+++ src/_06_Card_Game/CardDealer.java
@@ -4,14 +4,45 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Random;
 
+import javax.swing.JOptionPane;
 import javax.swing.text.StyledEditorKit.ForegroundAction;
 
 public class CardDealer {
     
     static ArrayList<Card> deck;
+    static ArrayList<Card> hand;
+    static ArrayList<Card> dealer;
     static Suit[] suits = new Suit[4];
     static Rank[] ranks = new Rank[13];
     public static void main(String[] args) {
+	Boolean contPlay = true;
+	Boolean nonStand = true;
+	deck = new ArrayList<Card>();
+	hand = new ArrayList<Card>();
+	dealer = new ArrayList<Card>();
+    newDeck();
+	while(contPlay) {	
+	if(deck.size() < 10) {
+		newDeck();
+	}
+    dealCard(hand);
+    dealCard(dealer);
+    dealCard(hand);
+    Card c = deck.get(0);
+    deck.remove(0);
+    while(nonStand) {
+    	String choice = JOptionPane.showInputDialog("You have " + getPlayerHand() + ". The dealer's visible card is " + dealer.get(0).getRank() + " of " + dealer.get(0).getSuit() + "\nType H to hit, S to stand");
+    	if(choice.equals("H")) {
+    		dealCard(hand);
+    	}
+    	else if (choice.equals("S")) {
+    		nonStand = false;
+    	}
+    }
+    
+    }
+    }
+    public static void newDeck() {
     	Random rand = new Random();
     	Card card;
      	suits[0] = Suit.Hearts;
@@ -34,14 +65,61 @@ public class CardDealer {
     	int suitNum = 0;
     	int rankNum = 0;
     	for (int i = 0; i < 53; i++) {
-    		suitNum = rand.nextInt(4)+1;
-    		rankNum = rand.nextInt(12)+2;
+    		suitNum = rand.nextInt(4);
+    		rankNum = rand.nextInt(13);
     		
     		card = new Card(ranks[rankNum], suits[suitNum]);
     		while (deck.contains(card)) {
-				type type = (type) en.nextElement();
-				
+    			suitNum = rand.nextInt(4)+1;
+        		rankNum = rand.nextInt(12)+2;
+				card = new Card(ranks[rankNum], suits[suitNum]);
 			}
+    		deck.add(card);
 		}
-	}
+    }
+    public static void shuffle() {
+    	Card placeholder;
+    	Random rand = new Random();
+    	int place = 0;
+    	for(int i = 0; i < deck.size(); i++) {
+    		place = rand.nextInt(52);
+    		placeholder = deck.get(place);
+    		deck.set(place, deck.get(i));
+    		deck.set(i, placeholder);
+    	}
+    }
+    public static void dealCard(ArrayList <Card> cardHand) {
+    	Random rand = new Random();
+    	int selection = rand.nextInt(deck.size());
+    	cardHand.add(deck.get(selection));
+    	if(cardHand.equals(hand)) {
+    	JOptionPane.showMessageDialog(null, "Dealt " + deck.get(selection).getRank() + " of " + deck.get(selection).getSuit() + " to player");
+    	}
+    	if(cardHand.equals(dealer))) {
+        	JOptionPane.showMessageDialog(null, "Dealt " + deck.get(selection).getRank() + " of " + deck.get(selection).getSuit() + " to dealer");
+        	}
+    	deck.remove(selection);
+    }
+    public static void clearHands() {
+    	hand.clear();
+    	dealer.clear();
+    }
+    public static int getPlayerHand() {
+		int returned = 0;
+    	for(int i = 0; i < hand.size(); i ++) {
+    		returned += hand.get(i).getRank().getRankValue();
+    	}
+    	while(returned > 21){
+    		for(int i = 0; i <hand.size(); i++) {
+    			if(hand.get(i).getRank()==Rank.ACE) {
+    				returned-=10;
+    				if(returned <= 21) {
+    					break;
+    				}
+    			}
+    		}
+    	}
+		return returned;
+    }
+	
 }
--- src/_06_Card_Game/Rank.java
+++ src/_06_Card_Game/Rank.java
@@ -1,7 +1,7 @@
 package _06_Card_Game;
 
 public enum Rank {
-	TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(11), QUEEN(12), KING(13), ACE(14);
+	TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10), ACE(11);
 	
 	private final int value;
 	
